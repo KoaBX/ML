ad51de07541a974f9a66748f85a9d3a59cabdba6
KoaBX <143489354+KoaBX@users.noreply.github.com>
2025-05-04 23:50:27 +0800

    Update g7_final_coding.py

--- g7_final_coding.py
+++ g7_final_coding.py
@@ -284,7 +284,7 @@ else:
     st.warning("Only one cluster detected. Evaluation metrics not available.")
 
 # Visualize clusters using PCA
-fig, axs = plt.subplots(1, 1, figsize=(18, 10))  # 2 rows, 3 columns
+fig, axs = plt.subplots(figsize=(18, 10), squeeze=False)
 axs[0, 0].scatter(X_pca[:, 0], X_pca[:, 1], c=labels, cmap='tab10')
 plt.tight_layout()
 st.pyplot(fig)
