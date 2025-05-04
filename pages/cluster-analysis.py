3b364fd8ee9df1e050a95340dcd8a336bce6612a
KoaBX <143489354+KoaBX@users.noreply.github.com>
2025-05-05 03:21:07 +0800

    Update cluster-analysis.py

--- pages/cluster-analysis.py
+++ pages/cluster-analysis.py
@@ -15,7 +15,9 @@ if uploaded_file is not None:
 else:
     st.warning("Please upload a CSV file to proceed.")
     st.stop()  # Prevents the rest of the app from running until a file is uploaded
-    
+
+X_scaled = scaler.fit_transform(df)
+
 # 6. Cluster Analysis and Visualization
 # Interpret KMeans Clusters
 st.subheader("KMeans Cluster Centers Analysis")
